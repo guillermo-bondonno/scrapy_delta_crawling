e43c698099cc700dcea768a8115b33faffdbc2cb
guillermo-bondonno <guillermo.bondonno@zyte.com>
2024-07-03 15:29:25 -0300

    setup fix

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
-def parse_requirements(filename):
-    with open(filename) as f:
-        return f.read().splitlines()
-
 setup(
     name='scrapy_delta_crawling',
     version='0.1.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    install_requires=parse_requirements('requirements.txt'),
+    install_requires=[
+        'scrapy',
+        'scrapinghub',
+    ],
 )
